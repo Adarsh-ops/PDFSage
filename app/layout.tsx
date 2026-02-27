53593c200f9f15b013e6a3e21f1c4de238d7421a
Adarsh-Nagar-Ops <adarshnagar247@gmail.com>
2026-02-27 18:24:00 +0530

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
 import Navigation from "@/components/navigation";
+import {Analytics} from '@vercel/analytics/next'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
         >
           <Navigation />
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
